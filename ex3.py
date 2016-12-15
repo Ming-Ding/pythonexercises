3129e769f48fb36ce5710d22ef9964bdd5766e5e
Ming-Ding <joding0000@hotmail.com>
2016-12-15 10:05:42 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,23 @@
+# type "I will now ..."
+print "I will now count my chickens:"
+# type hens, do the math
+print "hens", 25 + 30 / 6
+print "roosters", 100 - 25 * 3 % 4
+print "Now I will count the eggs:"
+# do the math
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+# type the question, then do the math
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+# type the question, do the math and then do the true or false 
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal", 5 <= -2
